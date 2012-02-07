6ac60fac918cf061225490552ebf09429f81ecdd
David McClosky <dmcc@bigasterisk.com>
2012-02-07 13:56:35 -0800

    Better error messages when using external POS tags. If you specify a tag which isn't in terms.txt, you'll get a warning and the tag will be ignored.

--- first-stage/PARSE/ExtPos.C
+++ first-stage/PARSE/ExtPos.C
@@ -1,5 +1,6 @@
 
 #include "ExtPos.h"
+#include "utils.h"
 #include <sstream>
 
 /*
@@ -30,14 +31,23 @@ read(ifstream* isp,SentRep& sr)
     }
     vector<const Term*> vt;
     stringstream ss(buf);
-    string wrd;
+    ECString wrd;
     ss>>wrd;  //First entry on each line is the word, not a tag.
+    escape_parens(wrd);
+    if (wrd != sr[i].lexeme()) {
+      cerr << "wrd:            '" << wrd << "'" << endl;
+      cerr << "sr[i].lexeme(): '" << sr[i].lexeme() << "'" << endl;
+    }
     assert(wrd==sr[i].lexeme());
     while(ss){
       ss>>wrd;
       if (!ss) break;
       const Term* trm=Term::get(wrd);
-      vt.push_back(trm);
+      if (trm) {
+        vt.push_back(trm);
+      } else {
+        cerr << "Warning: Haven't seen term '" << wrd << "' in terms.txt" << endl;
+      }
     }
     push_back(vt);
   }
