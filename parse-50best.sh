8bcc37a071cd80d65f0627d89228b681df1813eb
David McClosky <dmcc@bigasterisk.com>
2013-03-11 15:55:30 -0400

    parse-50best.sh: This script works out-of-the-box again.

--- parse-50best.sh
+++ parse-50best.sh
@@ -12,5 +12,7 @@
 # under the License.
 
 # RERANKDATA=ec50-connll-ic-s5
-RERANKDATA=ec50-f050902-lics5
-first-stage/PARSE/parseIt -l999 -N50 -K first-stage/DATA/EN/ $* | second-stage/programs/features/best-parses -l -m 1 second-stage/features/$RERANKDATA/traindev-feat.gz second-stage/features/$RERANKDATA/cvlm-l1-c10-Pyx1-ns-1/traindev-weights.gz
+# RERANKDATA=ec50-f050902-lics5
+MODELDIR=second-stage/models/ec50spfinal
+ESTIMATORNICKNAME=cvlm-l1c10P1
+first-stage/PARSE/parseIt -l399 -N50 first-stage/DATA/EN/ $* | second-stage/programs/features/best-parses -l -m 1 $MODELDIR/features.gz $MODELDIR/$ESTIMATORNICKNAME-weights.gz
