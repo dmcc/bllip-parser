5febdb90960994217594f1c235250b27352bfee4
David McClosky <dmcc@bigasterisk.com>
2015-01-11 22:17:56 -0500

    python/bllipparser/ParsingShell.py: improved Stanford Dependencies support

--- python/bllipparser/ParsingShell.py
+++ python/bllipparser/ParsingShell.py
@@ -10,22 +10,42 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+"""Simple interactive shell for viewing parses. To run:
+
+    python -mbllipparser.ParsingShell /path/to/model/
+
+Optional dependencies:
+If you have NLTK installed, you'll be able to use the 'visual' command
+which shows constituency trees.
+If you have PyStanfordDependencies installed, you'll be able to use the
+'sdparse' command. Dependencies are shown in CoNLL-X format, though ASCII
+trees will be shown if you have the asciitree package."""
 import sys
 from cmd import Cmd
-import nltk.tree
+import importlib
+def import_maybe(module_name):
+    "Import a module and return it if available, otherwise returns None."
+    try:
+        return importlib.import_module(module_name)
+    except ImportError:
+        return None
+
 try:
+    import nltk.tree
     import nltk.draw.tree
-    have_tree_drawing = False
     read_nltk_tree = nltk.tree.Tree.parse
-    have_tree_drawing = True
+    have_nltk_tree_drawing = True
 except ImportError:
-    have_tree_drawing = False
-except AttributeError:
+    have_nltk_tree_drawing = False
+except AttributeError: # handle NLTK API changes
     try:
         read_nltk_tree = nltk.tree.Tree.fromstring
-        have_tree_drawing = True
+        have_nltk_tree_drawing = True
     except AttributeError:
-        have_tree_drawing = False
+        have_nltk_tree_drawing = False
+
+StanfordDependencies = import_maybe('StanfordDependencies')
+asciitree = import_maybe('asciitree')
 
 from bllipparser.RerankingParser import RerankingParser
 
@@ -34,18 +54,26 @@ from bllipparser.RerankingParser import RerankingParser
 class ParsingShell(Cmd):
     def __init__(self, model):
         Cmd.__init__(self)
-        self.prompt = 'rrp> '
-        print "Loading models..."
+        self.prompt = 'bllip> '
         if model is None:
+            print "Warning: no parsing model to load."
+            print "Specify with: python -mbllipparser.ParsingShell " + \
+                  "/path/to/model/"
             self.rrp = None
         else:
+            sys.stdout.write("Loading models... ")
+            sys.stdout.flush()
             self.rrp = RerankingParser.from_unified_model_dir(model)
+            print "done!"
+        print "Enter a sentence to see its parse or 'help' for more options."
         self.last_nbest_list = []
+        self.options = {}
+        self.sd = None
 
     def do_visual(self, text):
         """Use reranking parser to parse text.  Visualize top parses from
         parser and reranker."""
-        if not have_tree_drawing:
+        if not have_nltk_tree_drawing:
             print "Can't visualize without NLTK installation."
             return
 
@@ -89,7 +117,7 @@ class ParsingShell(Cmd):
         """Usage: visualnbest [start] stop
         Visualizes all parses from start-stop in the n-best list.
         Sentence must already be parsed."""
-        if not have_tree_drawing:
+        if not have_nltk_tree_drawing:
             print "Can't visualize without NLTK installation."
             return
 
@@ -144,15 +172,69 @@ class ParsingShell(Cmd):
         self.got_nbest_list(nbest_list)
         self.print_parses()
 
+    def do_set(self, text):
+        """Set an option. Syntax: set option-name option-value
+        Current options:
+
+        sdversion - version of Stanford CoreNLP to use for Stanford
+                    Dependencies
+        sdbackend - backend to use for Stanford Dependencies conversion
+        sdvis     - how to draw Stanford Dependencies (can be conll or
+                    asciitree). Default is to use asciitree if available."""
+        pieces = text.split()
+        if len(pieces) != 2:
+            print "Syntax: set option-name option-value"
+            return
+        key, value = pieces
+        self.options[key] = value
+        print "set %r = %r" % (key, value)
+        if key.startswith('sd'):
+            # reset it so it can be reloaded
+            self.sd = None
+
     def do_sdparse(self, text):
         """Use reranking parser to parse text, then show the
-        output as Stanford Dependencies in CoNLL format.  Requires
-        PyStanfordDependencies."""
-        nbest_list = self.parse(text)
+        output as Stanford Dependencies in CoNLL format. Requires
+        PyStanfordDependencies. You may want to use the 'set' command
+        to set the sdversion, sdbackend, and sdvis options
+        (see 'help set')."""
+        if not StanfordDependencies:
+            print "Can't show dependencies without " \
+                  "PyStanfordDependencies installation."
+            return
 
-        tokens = nbest_list[0].ptb_parse.sd_tokens()
-        for token in tokens:
-            print token.as_conll()
+        try:
+            # load SD on demand so user can set version options before
+            self.load_stanford_dependencies()
+            nbest_list = self.parse(text)
+
+            parser_tree = nbest_list.get_parser_best().ptb_parse
+            reranker_tree = nbest_list.get_reranker_best().ptb_parse
+
+            parser_tokens = parser_tree.sd_tokens(sd_converter=self.sd)
+            reranker_tokens = reranker_tree.sd_tokens(sd_converter=self.sd)
+        except StanfordDependencies.JavaRuntimeVersionError, jrve:
+            # load_stanford_dependencies and sd_tokens potentially throw
+            # this
+            print 'JavaRuntimeVersionError:', jrve
+            print
+            print "Try running: 'set sdversion 3.4.1'"
+            if self.options.get('sdbackend', 'jpype') == 'jpype':
+                print "Also, since you're using the jpype backend, " + \
+                      "you'll need to restart first."
+            return
+
+        if parser_tokens == reranker_tokens:
+            print 'Parser and reranker:'
+        else:
+            print 'Parser:'
+
+        self.visualize_sd_tokens(parser_tokens)
+
+        if parser_tokens != reranker_tokens:
+            print
+            print 'Reranker:'
+            self.visualize_sd_tokens(reranker_tokens)
 
     def default(self, text):
         if text == 'EOF':
@@ -176,6 +258,16 @@ class ParsingShell(Cmd):
             print reranker_top_parse.ptb_parse.pretty_string()
         print
 
+    def visualize_sd_tokens(self, tokens):
+        # eventually, we may add dot/xdot support
+        sdvis = self.options.get('sdvis', 'asciitree').lower()
+        use_asciitree = sdvis == 'asciitree'
+        if use_asciitree and asciitree:
+            print tokens.as_asciitree()
+        else:
+            for token in tokens:
+                print token.as_conll()
+
     def got_nbest_list(self, nbest_list):
         nbest_list.sort_by_reranker_scores()
         self.last_nbest_list = nbest_list
@@ -188,6 +280,14 @@ class ParsingShell(Cmd):
             self.got_nbest_list(nbest_list)
 
         return self.last_nbest_list
+
+    def load_stanford_dependencies(self):
+        if self.sd:
+            return
+        kwargs = dict(version=self.options.get('sdversion'))
+        if 'sdbackend' in self.options:
+            kwargs['backend'] = self.options['sdbackend']
+        self.sd = StanfordDependencies.get_instance(**kwargs)
 
 def main(shell_class=ParsingShell):
     if len(sys.argv) > 1:
