61ad21cd46556d0c2b017407463e6325333f9507
David McClosky <dmcc@bigasterisk.com>
2015-02-06 16:50:23 -0500

    python/examples: Add some tree/parsing examples

---
+++ python/examples/parse_constrained.py
@@ -0,0 +1,55 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.  You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Example of how to parse with arbitrary constraints. The consistent()
+function can be replaced with any other predicate. Note that depending
+on your constraints, you may need to expand the n-best first before you
+find a tree that matches."""
+
+def parse_constrained(rrp, sentence, constraints):
+    """Example of how to find the highest scoring parse in the n-best list
+    match certain span constraints. constraints is a mapping from spans
+    (start, end) to a list of allowed labels for that span.  Returns None
+    if no parses match."""
+    def consistent(tree, constraints):
+        mapping = {}
+        for subtree in tree.all_subtrees():
+            mapping[subtree.span()] = subtree.label
+        for span, allowed_labels in constraints.items():
+            if mapping.get(span) not in allowed_labels:
+                return False
+        return True
+
+    nbest_list = rrp.parse(sentence)
+    for item in nbest_list:
+        if consistent(item.ptb_parse, constraints):
+            return item.ptb_parse
+    else:
+        return None
+
+if __name__ == "__main__":
+    # this needs to be run from the root of the repository since it has
+    # a relative path to the parsing model
+
+    from bllipparser import RerankingParser, Tree
+    rrp = RerankingParser()
+    rrp.load_parser_model('first-stage/DATA/EN')
+
+    # the constraint means: there must be a VP from [1,5)
+    # (i.e., left ... Falklands)
+    # this encourages the parser to pick "left" as the main verb
+    constraints = {(1, 5) : ['VP']}
+    print parse_constrained(rrp, 'British left waffles on Falklands .'.split(),
+                            constraints)
+    # if we parse without constraints, we get that the main verb is "waffles"
+    print parse_constrained(rrp, 'British left waffles on Falklands .'.split(),
+                            {})
---
+++ python/examples/tree_fun.py
@@ -0,0 +1,61 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.  You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Miscellaneous tree functions obscure enough to not put into the Tree
+class."""
+
+def replace_leaves(source_tree, target_tree):
+    """Given two Tree objects, replace all the leaves in source_tree
+    with those in the target_tree Tree (admittedly a strange thing to do,
+    but this does have uses)."""
+    leaves = iter(target_tree.tokens())
+    for subtree in source_tree.all_subtrees():
+        if subtree.is_preterminal():
+            subtree.token = leaves.next()
+
+def is_prepreterminal(node):
+    """Returns True iff all children of this node are preterminals."""
+    subtrees = node.subtrees()
+    return len(subtrees) > 0 and \
+        all(subtree.is_preterminal() for subtree in subtrees)
+
+def labeled_spans(tree):
+    """Yields all labeled span in the tree: (start, end, label)."""
+    for subtree in tree.all_subtrees():
+        start, end = subtree.span()
+        yield (start, end, subtree.label)
+
+def strip_function_tags(tree):
+    """Removes all function tags from the tree."""
+    for subtree in tree.all_subtrees():
+        subtree.label_suffix = ''
+
+if __name__ == "__main__":
+    from bllipparser import Tree
+
+    tree = Tree('''(S1 (S (VP (VBZ Swears) (SBAR (SBAR (S (NP-SBJ
+    (PRP she)) (VP (VBD recognized) (NP (PRP$ his) (NN voice))))) (, ,)
+    (SBAR (IN that) (S (NP-SBJ (NNP Tim)) (VP (VBD fired) (, ,) ('' ')
+    (S (S (NP-SBJ (PRP It)) (VP (VBZ 's) (NP-PRD (PRP$ my) (NN money))))
+    (CC and) (S (NP-SBJ (PRP I)) (VP (VBP want) (NP (PRP it))))) 
+    ('' ')))))) (. !)))''')
+
+    for labeled_span in labeled_spans(tree):
+        print labeled_span
+
+    print '---'
+    for subtree in tree.all_subtrees():
+        print is_prepreterminal(subtree), subtree
+    print '---'
+    strip_function_tags(tree)
+    for subtree in tree.all_subtrees():
+        print is_prepreterminal(subtree), subtree
