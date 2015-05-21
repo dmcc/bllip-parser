3e9a2ec6980c6d3f4c40a6e4a2d555dd47816dec
David McClosky <dmcc@bigasterisk.com>
2015-05-21 18:54:06 -0400

    setup.py: PEP8-ification

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-import os, subprocess
+import os
+import subprocess
 from os.path import join, exists
 
 # If you are creating a sdist from the full bllipparser code base, you
@@ -10,7 +11,7 @@ from os.path import join, exists
 # these dependencies.
 
 def run(args):
-    print "Running %r" % ' '.join(map(str, args))
+    print("Running %r" % ' '.join(map(str, args)))
     subprocess.check_call(args)
 
 parser_base = 'first-stage/PARSE/'
@@ -34,30 +35,33 @@ def maybe_run_swig(wrapper_filename, module_name, base_directory,
         if not newer:
             return
 
-    print 'Generating', module_name, 'SWIG wrapper files'
+    print('Generating', module_name, 'SWIG wrapper files')
     run(['swig', '-python', '-c++', '-module',
-        module_name, '-I' + base_directory,
-        '-Wall', '-classic', '-outdir', 'python/bllipparser',
-        '-o', wrapper_filename, swig_filename])
+         module_name, '-I' + base_directory,
+         '-Wall', '-classic', '-outdir', 'python/bllipparser',
+         '-o', wrapper_filename, swig_filename])
 
 # generate parser SWIG files if needed
 maybe_run_swig(parser_wrapper_full, 'CharniakParser', parser_base,
-    extra_deps=[join(parser_base, 'SimpleAPI.' + suffix) for suffix in 'Ch'])
+               extra_deps=[join(parser_base, 'SimpleAPI.' + suffix)
+                           for suffix in 'Ch'])
 
-parser_sources = [join(parser_base, src) for src in
-    (parser_wrapper, 'Bchart.C', 'BchartSm.C', 'Bst.C', 'FBinaryArray.C',
-     'CntxArray.C', 'ChartBase.C', 'ClassRule.C', 'ECArgs.C', 'Edge.C',
-     'EdgeHeap.C', 'ExtPos.C', 'Feat.C', 'Feature.C', 'FeatureTree.C',
-     'Field.C', 'FullHist.C', 'GotIter.C', 'InputTree.C', 'Item.C',
-     'Link.C', 'Params.C', 'ParseStats.C', 'SentRep.C', 'ScoreTree.C',
-     'Term.C', 'TimeIt.C', 'UnitRules.C', 'ValHeap.C', 'edgeSubFns.C',
-     'ewDciTokStrm.C', 'extraMain.C', 'fhSubFns.C', 'headFinder.C',
-     'headFinderCh.C', 'utils.C', 'MeChart.C')
-]
+parser_sources = (parser_wrapper, 'Bchart.C', 'BchartSm.C', 'Bst.C',
+                  'FBinaryArray.C', 'CntxArray.C', 'ChartBase.C',
+                  'ClassRule.C', 'ECArgs.C', 'Edge.C', 'EdgeHeap.C',
+                  'ExtPos.C', 'Feat.C', 'Feature.C', 'FeatureTree.C',
+                  'Field.C', 'FullHist.C', 'GotIter.C', 'InputTree.C',
+                  'Item.C', 'Link.C', 'Params.C', 'ParseStats.C',
+                  'SentRep.C', 'ScoreTree.C', 'Term.C', 'TimeIt.C',
+                  'UnitRules.C', 'ValHeap.C', 'edgeSubFns.C',
+                  'ewDciTokStrm.C', 'extraMain.C', 'fhSubFns.C',
+                  'headFinder.C', 'headFinderCh.C', 'utils.C',
+                  'MeChart.C')
+parser_sources = [join(parser_base, src) for src in parser_sources]
 
 parser_module = Extension('bllipparser._CharniakParser',
-    sources=parser_sources, include_dirs=[parser_base],
-    libraries=['stdc++'])
+                          sources=parser_sources, include_dirs=[parser_base],
+                          libraries=['stdc++'])
 
 reranker_base = 'second-stage/programs/features/'
 reranker_wrapper = 'swig/wrapper.C'
@@ -73,33 +77,33 @@ if not exists(reranker_read_tree_full):
     run(['flex', '-o' + reranker_read_tree_full,
         reranker_read_tree_full.replace('.cc', '.l')])
 
-reranker_sources = [reranker_base + src for src in 
-    (reranker_wrapper, 'simple-api.cc', 'heads.cc', reranker_read_tree,
-     'sym.cc')]
+reranker_sources = [join(reranker_base, src) for src in
+                    (reranker_wrapper, 'simple-api.cc', 'heads.cc',
+                     reranker_read_tree, 'sym.cc')]
 
 reranker_module = Extension('bllipparser._JohnsonReranker',
-    sources=reranker_sources,
-    extra_compile_args=['-iquote', reranker_base, '-DSWIGFIX'])
+                            sources=reranker_sources,
+                            extra_compile_args=['-iquote', reranker_base,
+                                                '-DSWIGFIX'])
 
 setup(name='bllipparser',
-    version='2015.01.11',
-    description='Python bindings for the BLLIP natural language parser',
-    long_description=file('README-python.rst').read(),
-    author='Eugene Charniak, Mark Johnson, David McClosky, many others',
-    maintainer='David McClosky',
-    maintainer_email='notsoweird+pybllipparser@gmail.com',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Operating System :: POSIX',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    ],
-    url='http://github.com/BLLIP/bllip-parser',
-    license='Apache 2.0',
-    platforms=['POSIX'],
-    ext_modules=[parser_module, reranker_module],
-    packages=['bllipparser'],
-    package_dir={'bllipparser' : 'python/bllipparser'},
-)
+      version='2015.01.11',
+      description='Python bindings for the BLLIP natural language parser',
+      long_description=file('README-python.rst').read(),
+      author='Eugene Charniak, Mark Johnson, David McClosky, many others',
+      maintainer='David McClosky',
+      maintainer_email='notsoweird+pybllipparser@gmail.com',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      url='http://github.com/BLLIP/bllip-parser',
+      license='Apache 2.0',
+      platforms=['POSIX'],
+      ext_modules=[parser_module, reranker_module],
+      packages=['bllipparser'],
+      package_dir={'bllipparser': 'python/bllipparser'})
